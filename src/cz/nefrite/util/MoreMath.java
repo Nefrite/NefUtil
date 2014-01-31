96d9658deae725aa0df0d47a776ac053d5acbf11
drozd01 <drozd01@VL10436.pre.cz>
2014-01-31 13:43:10 +0100

    úprava Javadoc

--- src/cz/nefrite/util/MoreMath.java
+++ src/cz/nefrite/util/MoreMath.java
@@ -4,6 +4,7 @@ package cz.nefrite.util;
  * Třída z knihy Vývoj her v jazyku Java
  * *** com.brackeen.javagamebook.util *** 
  * @author David Brackeen
+ * @author Jirka Drozd
  */
 public class MoreMath {
 	
@@ -29,6 +30,9 @@ public class MoreMath {
 
 	/**
 	 * Funkce kosinu, kde úhel je v rozsahu 0 až 4096 namísto 0 až 2PI.
+	 * 
+	 * @param angle úhel v rozsahu 0 až 4096 bodů
+	 * @return kosinus úhlu
 	 */
 	public static float cos(int angle) {
 		return sinTable[(HALF_PI - angle) & TABLE_SIZE_MASK];
@@ -36,6 +40,9 @@ public class MoreMath {
 
 	/**
 	 * Funkce sinu, kde úhel je v rozsahu 0 až 4096 namísto 0 až 2PI.
+	 * 
+	 * @param angle úhel v rozsahu 0 až 4096 bodů
+	 * @return sinus úhlu 
 	 */
 	public static float sin(int angle) {
 		return sinTable[angle & TABLE_SIZE_MASK];
@@ -43,6 +50,9 @@ public class MoreMath {
 
 	/**
 	 * Převede úhel v radiánech na přepočtový systém (0 až 2pi se stává 0 až 4096).
+	 * 
+	 * @param angleInRadians úhel v radiánech
+	 * @return úhel v přepočtových bodech
 	 */
 	public static int angleConvert(float angleInRadians) {
 		return (int) (angleInRadians * CONVERSION_FACTOR);
@@ -51,7 +61,9 @@ public class MoreMath {
 	/**
         Returns the sign of the number. Returns -1 for negative,
         1 for positive, and 0 otherwise.
-    */
+		* @param v number as {@code short}
+		* @return -1 if negative | 0 if zero | 1 if positive 
+		*/
     public static int sign(short v) {
         return (v>0)?1:(v<0)?-1:0;
     }
@@ -60,7 +72,9 @@ public class MoreMath {
     /**
         Returns the sign of the number. Returns -1 for negative,
         1 for positive, and 0 otherwise.
-    */
+		* @param v number as {@code int}
+		* @return -1 if negative | 0 if zero | 1 if positive 
+		*/
     public static int sign(int v) {
         return (v>0)?1:(v<0)?-1:0;
     }
@@ -69,7 +83,9 @@ public class MoreMath {
     /**
         Returns the sign of the number. Returns -1 for negative,
         1 for positive, and 0 otherwise.
-    */
+		* @param v number as {@code long}
+		* @return -1 if negative | 0 if zero | 1 if positive  
+		*/
     public static int sign(long v) {
         return (v>0)?1:(v<0)?-1:0;
     }
@@ -78,7 +94,9 @@ public class MoreMath {
     /**
         Returns the sign of the number. Returns -1 for negative,
         1 for positive, and 0 otherwise.
-    */
+		* @param v number as {@code float}
+		* @return -1 if negative | 0 if zero | 1 if positive  
+		*/
     public static int sign(float v) {
         return (v>0)?1:(v<0)?-1:0;
     }
@@ -87,7 +105,9 @@ public class MoreMath {
     /**
         Returns the sign of the number. Returns -1 for negative,
         1 for positive, and 0 otherwise.
-    */
+		* @param v  number as {@code double}
+		* @return -1 if negative | 0 if zero | 1 if positive 
+		*/
     public static int sign(double v) {
         return (v>0)?1:(v<0)?-1:0;
     }
@@ -98,7 +118,9 @@ public class MoreMath {
         Contrary to the java.lang.Math ceil function, this
         function takes a float as an argument, returns an int
         instead of a double, and does not consider special cases.
-    */
+		* @param f  number as {@code float}
+		* @return  
+		*/
     public static int ceil(float f) {
         if (f > 0) {
             return (int)f + 1;
@@ -114,7 +136,9 @@ public class MoreMath {
         Contrary to the java.lang.Math floor function, this
         function takes a float as an argument, returns an int
         instead of a double, and does not consider special cases.
-    */
+		* @param f  number as {@code float}
+		* @return  
+		*/
     public static int floor(float f) {
         if (f >= 0) {
             return (int)f;
@@ -127,7 +151,9 @@ public class MoreMath {
 
     /**
         Returns true if the specified number is a power of 2.
-    */
+		* @param n 
+		* @return  
+		*/
     public static boolean isPowerOfTwo(int n) {
         return ((n & (n-1)) == 0);
     }
@@ -135,7 +161,9 @@ public class MoreMath {
 
     /**
         Gets the number of "on" bits in an integer.
-    */
+		* @param n
+		* @return  
+		*/
     public static int getBitCount(int n) {
         int count = 0;
         while (n > 0) {
